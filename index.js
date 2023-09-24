495a7d0031e696fda171ac0a9842b09a0bf87ab5
猪易零零一 <145171620+zhue001@users.noreply.github.com>
2023-09-24 12:56:33 +0800

    添加自定义按钮图标宽高

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 if ((typeof window == 'object' && !window.zhue) || (typeof global == 'object' && !global.zhue)) window ? window.zhue = {} : global.zhue = {}
 zhue.showInfoBox = function(t, e) {
   function n(t) {
-    var e = t.replace("#", ""), n = parseInt(e.substr(0, 2), 16), r = parseInt(e.substr(2, 2), 16), o = parseInt(e.substr(4, 2), 16), a = 255 - n, l = 255 - r, s = 255 - o, i = "#" + a.toString(16).padStart(2, "0") + l.toString(16).padStart(2, "0") + s.toString(16).padStart(2, "0");
-    return i;
+    var e = t.replace("#", ""), n = parseInt(e.substr(0, 2), 16), r = parseInt(e.substr(2, 2), 16), o = parseInt(e.substr(4, 2), 16), a = 255 - n, i = 255 - r, l = 255 - o, s = "#" + a.toString(16).padStart(2, "0") + i.toString(16).padStart(2, "0") + l.toString(16).padStart(2, "0");
+    return s;
   }
   function r(t, e) {
     var n = t.replace("#", ""), r = parseInt(n.substr(0, 2), 16), o = parseInt(n.substr(2, 2), 16), a = parseInt(n.substr(4, 2), 16);
     r = Math.max(0, r - e), o = Math.max(0, o - e), a = Math.max(0, a - e);
-    var l = "#" + r.toString(16).padStart(2, "0") + o.toString(16).padStart(2, "0") + a.toString(16).padStart(2, "0");
-    return l;
+    var i = "#" + r.toString(16).padStart(2, "0") + o.toString(16).padStart(2, "0") + a.toString(16).padStart(2, "0");
+    return i;
   }
   var o = document.createElement("div");
   o.classList.add("info-box"), o.style.position = "fixed", o.style.top = "50%", o.style.left = "50%", 
@@ -16,12 +16,12 @@ zhue.showInfoBox = function(t, e) {
   o.style.border = e.border, o.style.padding = "3mm";
   var a = document.createElement("h2");
   a.innerText = e.title;
-  var l = document.createElement("img");
-  l.src = e.icon;
-  var s = document.createElement("p");
-  s.innerText = e.content, a.style.color = n(e.bgColor), s.style.color = n(e.bgColor), 
-  o.appendChild(a), o.appendChild(l), o.appendChild(s);
-  var i = document.createElement("div");
+  var i = document.createElement("img");
+  i.src = e.icon;
+  var l = document.createElement("p");
+  l.innerText = e.content, a.style.color = n(e.bgColor), l.style.color = n(e.bgColor), 
+  o.appendChild(a), o.appendChild(i), o.appendChild(l);
+  var s = document.createElement("div");
   e.buttons.forEach(function(t) {
     var e;
     if (t.linkhref ? (e = document.createElement("a"), e.setAttribute("href", t.linkhref), 
@@ -38,9 +38,10 @@ zhue.showInfoBox = function(t, e) {
     }), t.icon) {
       var n = document.createElement("img");
       n.src = t.icon, n.style.marginRight = "0.5mm", n.style.width = t.iconsize[0], n.style.height = t.iconsize[1], 
+      n.style.width = t.iconsize[0] ? t.iconsize[0] : "8px", n.style.height = t.iconsize[1] ? t.iconsize[1] : "8px", 
       e.appendChild(n);
     }
     var a = document.createElement("span");
-    a.innerText = t.name, e.appendChild(a), i.appendChild(e);
-  }), o.appendChild(i), t.appendChild(o);
-}
+    a.innerText = t.name, e.appendChild(a), s.appendChild(e);
+  }), o.appendChild(s), t.appendChild(o);
+};
