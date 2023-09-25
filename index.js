1fbda0b94059ce8ae081c3b77f669b8a2808ea3d
猪易零零一 <145171620+zhue001@users.noreply.github.com>
2023-09-26 06:19:34 +0800

    格式化、修复按钮触碰无法背景色加深的bug

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
 if ((typeof window == 'object' && !window.zhue) || (typeof global == 'object' && !global.zhue)) window ? window.zhue = {} : global.zhue = {}
 zhue.showInfoBox = function(t, e) {
-  function n(t) {
-    var e = t.replace("#", ""), n = parseInt(e.substr(0, 2), 16), r = parseInt(e.substr(2, 2), 16), o = parseInt(e.substr(4, 2), 16), a = 255 - n, i = 255 - r, l = 255 - o, s = "#" + a.toString(16).padStart(2, "0") + i.toString(16).padStart(2, "0") + l.toString(16).padStart(2, "0");
-    return s;
-  }
-  function r(t, e) {
-    var n = t.replace("#", ""), r = parseInt(n.substr(0, 2), 16), o = parseInt(n.substr(2, 2), 16), a = parseInt(n.substr(4, 2), 16);
-    r = Math.max(0, r - e), o = Math.max(0, o - e), a = Math.max(0, a - e);
-    var i = "#" + r.toString(16).padStart(2, "0") + o.toString(16).padStart(2, "0") + a.toString(16).padStart(2, "0");
-    return i;
-  }
-  var o = document.createElement("div");
-  o.classList.add("info-box"), o.style.position = "fixed", o.style.top = "50%", o.style.left = "50%", 
-  o.style.transform = "translate(-50%, -50%)", o.style.backgroundColor = e.bgColor, 
-  o.style.border = e.border, o.style.padding = "3mm";
-  var a = document.createElement("h2");
-  a.innerText = e.title;
-  var i = document.createElement("img");
-  i.src = e.icon;
-  var l = document.createElement("p");
-  l.innerText = e.content, a.style.color = n(e.bgColor), l.style.color = n(e.bgColor), 
-  o.appendChild(a), o.appendChild(i), o.appendChild(l);
-  var s = document.createElement("div");
-  e.buttons.forEach(function(t) {
-    var e;
-    if (t.linkhref ? (e = document.createElement("a"), e.setAttribute("href", t.linkhref), 
-    e.setAttribute("target", t.linktarget || "_blank")) : e = document.createElement("button"), 
-    e.style.backgroundColor = t.bgColor, e.style.padding = "1mm", e.style.marginRight = "1mm", 
-    e.style.color = t.textColor || "#000000", e.style.border = "none", e.style.cursor = "pointer", 
-    e.style.display = "inline-flex", e.style.alignItems = "center", e.addEventListener("mouseover", function() {
-      var t = e.style.backgroundColor, n = r(t, 10);
-      e.style.backgroundColor = n;
-    }), e.addEventListener("mouseout", function() {
-      e.style.backgroundColor = t.bgColor;
-    }), t.isCloseButton && e.addEventListener("click", function() {
-      o.remove();
-    }), t.icon) {
-      var n = document.createElement("img");
-      n.src = t.icon, n.style.marginRight = "0.5mm", n.style.width = t.iconsize[0], n.style.height = t.iconsize[1], 
-      n.style.width = t.iconsize[0] ? t.iconsize[0] : "8px", n.style.height = t.iconsize[1] ? t.iconsize[1] : "8px", 
-      e.appendChild(n);
+  function n(e) {
+    for (var t = e.substring(e.indexOf("(") + 1, e.lastIndexOf(")")).split(","), n = "#", o = 0; 3 > o; o++) {
+      var r = parseInt(t[o].trim()).toString(16);
+      n += 1 === r.length ? "0" + r : r
     }
-    var a = document.createElement("span");
-    a.innerText = t.name, e.appendChild(a), s.appendChild(e);
-  }), o.appendChild(s), t.appendChild(o);
-};
+    return n
+  }
+
+  function o(e) {
+    var t = e.replace("#", ""),
+      n = parseInt(t.substr(0, 2), 16),
+      o = parseInt(t.substr(2, 2), 16),
+      r = parseInt(t.substr(4, 2), 16),
+      a = 255 - n,
+      s = 255 - o,
+      l = 255 - r,
+      i = "#" + a.toString(16).padStart(2, "0") + s.toString(16).padStart(2, "0") + l.toString(16).padStart(2, "0");
+    return i
+  }
+
+  function r(e, t) {
+    var n = e.replace("#", ""),
+      o = parseInt(n.substr(0, 2), 16),
+      r = parseInt(n.substr(2, 2), 16),
+      a = parseInt(n.substr(4, 2), 16);
+    o = Math.max(0, o - t), r = Math.max(0, r - t), a = Math.max(0, a - t);
+    var s = "#" + o.toString(16).padStart(2, "0") + r.toString(16).padStart(2, "0") + a.toString(16).padStart(2, "0");
+    return s
+  }
+  var a = document.createElement("div");
+  a.classList.add("info-box"), a.style.position = "fixed", a.style.top = "50%", a.style.left = "50%", a.style.transform = "translate(-50%, -50%)", a.style.backgroundColor = t.bgColor, a.style.border = t.border, a.style.padding = "3mm";
+  var s = document.createElement("h2");
+  s.innerText = t.title;
+  var l = document.createElement("img");
+  l.src = t.icon;
+  var i = document.createElement("p");
+  i.innerText = t.content, s.style.color = o(t.bgColor), i.style.color = o(t.bgColor), a.appendChild(s), a.appendChild(l), a.appendChild(i);
+  var c = document.createElement("div");
+  t.buttons.forEach(function(e) {
+    var t;
+    if (e.linkhref ? (t = document.createElement("a"), t.setAttribute("href", e.linkhref), t.setAttribute("target", e.linktarget || "_blank")) : t = document.createElement("div"), t.style.backgroundColor = e.bgColor, t.style.padding = "1mm", t.style.marginRight = "1mm", t.style.color = e.textColor || "#000000", t.style.border = "none", t.style.cursor = "pointer", t.style.display = "inline-flex", t.style.alignItems = "center", t.addEventListener("mouseover", function() {
+        var e = t.style.backgroundColor,
+          o = r(n(e), 10);
+        t.style.backgroundColor = o
+      }), t.addEventListener("mouseout", function() {
+        t.style.backgroundColor = e.bgColor
+      }), e.isCloseButton && t.addEventListener("click", function() {
+        a.remove()
+      }), e.icon) {
+      var o = document.createElement("img");
+      o.src = e.icon, o.style.marginRight = "0.5mm", o.style.width = e.iconsize[0], o.style.height = e.iconsize[1], t.appendChild(o)
+    }
+    var s = document.createElement("span");
+    s.innerText = e.name, t.appendChild(s), c.appendChild(t)
+  }), a.appendChild(c), e.appendChild(a)
+}
